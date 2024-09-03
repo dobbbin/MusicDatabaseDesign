7658fb4a3b667e91324fb8b77141bfe599b8b17d
Alexander Dobbin <60853860+dobbbin@users.noreply.github.com>
2024-09-03 16:12:26 -0400

    Update q2.sql

--- q2.sql
+++ q2.sql
@@ -6,7 +6,7 @@ CREATE TABLE q2 (
     numsessions int NOT NULL
 );
 
-
+--The Person who participated in the most music sessions
 INSERT into q2
 SELECT  p.person_id as id, count(DISTINCT ms.sessions_id) as numsessions
 FROM People p LEFT JOIN Artist a ON p.person_id = a.member JOIN MusicSession ms ON ms.artist_id = a.artist_id
